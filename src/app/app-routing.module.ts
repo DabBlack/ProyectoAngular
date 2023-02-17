3a64d8869059bca1365136a93bbf451ca716639e
DabBlack <davidalgar2@gmail.com>
2023-02-17 13:06:05 +0100

    Registro y Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent},
+  { path: '', component: HomeComponent },
+  // { path: 'login', component: LoginComponent },
+  // { path: 'logout', component: LogoutComponent },
+  // { path: 'home', component: HomeComponent },
+  // { path: 'profile', component: ProfileComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
